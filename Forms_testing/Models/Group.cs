ef4a26ec2e986e30c9b474e09306f9cd95deba74
Илья Казютин <gorgutz1@yandex.ru>
2020-04-03 12:53:47 +0300

    Создана модель группы студентов

---
+++ Forms_testing/Models/Group.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forms_testing.Models
+{
+    class Group
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Course { get; set; }
+
+        public string Description { get; set; }
+    }
+}
